27dbe36be6795aaab114b813ff9423412e9a3d65
rody347 <rodrigoreis.veras@gmail.com>
2023-02-18 06:35:07 -0300

    Sorteador

--- sorteador_planilha.py
+++ sorteador_planilha.py
@@ -3,29 +3,19 @@ import os, sys
 import openpyxl 
 import random
 
+#Especificando a planilha
 book = openpyxl.load_workbook('Planilha de Clientes.xlsx')
+#Especificando a página
 clientes_page = book['Clientes']
-n = input('Insira a quantidade de clientes participantes: ')
-s = random.randrange(1,n)
-clientes = []
 
+#Pedindo a quantidade de elementos que serão sorteados
+n = input('Insira a quantidade de clientes participantes: ')
+#Criando o número sorteador aleatório e limitando ao tamanho do vetor
+s = random.randrange(1,n)
+
+#Criando o vetor com os participantes(puxando os dados da planilha) e iterando o sorteio
+clientes = []
 for col in clientes_page.iter_cols(min_row=2,max_col=1):
     for cell in col:
         clientes.append(cell.value)
 print(clientes[s])            
-
-
-
-
-# n = input('Insira a quantidade de clientes participantes: ')
-# #Gerando número aleatório
-# s = random.randrange(1,n)
-
-# def sorteador():
-#     s = random.randrange(1,12)
-    
-# def getting_clientes():
-#     clientes = []
-#     for col in clientes_page.iter_cols(min_row=2,max_col=1):
-#         for cell in col:
-#             clientes.append(cell.value)
