9efbda6c3361321da357c609f8eda7f1cf48f1dc
rody347 <rodrigoreis.veras@gmail.com>
2023-02-18 06:26:35 -0300

    Planilha fictícia com os dados de exemplo

---
+++ planilha.py
@@ -0,0 +1,25 @@
+# This Python file uses the following encoding: utf-8
+import os, sys
+import openpyxl
+
+book = openpyxl.Workbook()
+
+book.create_sheet('Clientes')
+
+#,telefone,endereco,produto
+s_page = book['Clientes']
+s_page.append(['Nome','telefone','endereco','produto participante'])
+s_page.append(['Paula ','telefone','endereco','produto'])
+s_page.append(['Fernanda ','telefone','endereco','produto'])
+s_page.append(['Renan ','telefone','endereco','produto'])
+s_page.append(['Amanda','telefone','endereco','produto'])
+s_page.append(['Pedro ','telefone','endereco','produto'])
+s_page.append(['Paulo ','telefone','endereco','produto'])
+s_page.append(['Gabriel ','telefone','endereco','produto'])
+s_page.append(['Guilherme ','telefone','endereco','produto'])
+s_page.append(['Lucas ','telefone','endereco','produto'])
+s_page.append(['Ricardo ','telefone','endereco','produto'])
+s_page.append(['Felipe ','telefone','endereco','produto'])
+s_page.append(['Filipe ','telefone','endereco','produto'])
+
+book.save('Planilha de Clientes.xlsx')
